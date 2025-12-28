a881743eabf2c090ce2587bcbe09d8ff86d6d0c5
Ali Allaoua <blackzero00ali00@gmail.com>
2025-12-28 20:35:52 +0100

    Fix issue with Tanstack Query when migrated to Bun

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,18 @@
-import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary";
 import { NotFound } from "./components/NotFound";
+import * as TanstackQuery from "./integrations/tanstack-query/root-provider";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
-	const queryClient = new QueryClient();
+	const rqContext = TanstackQuery.getContext();
 
 	const router = createRouter({
 		routeTree,
-		context: { queryClient },
+		context: {
+			...rqContext,
+		},
 		scrollRestoration: true,
 		defaultPreload: "intent",
 		defaultErrorComponent: DefaultCatchBoundary,
@@ -19,14 +21,8 @@ export const getRouter = () => {
 
 	setupRouterSsrQueryIntegration({
 		router,
-		queryClient,
+		queryClient: rqContext.queryClient,
 	});
 
 	return router;
 };
-
-// declare module '@tanstack/react-router' {
-// 	interface Register {
-// 		router: ReturnType<typeof getRouter>;
-// 	}
-// }
